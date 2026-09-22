046f87855f1a0073386f5904e3e07751e3e4dbbf
SladkyCitron <57183251+SladkyCitron@users.noreply.github.com>
2026-09-22 20:54:14 +0200

    refactor(frontend): rename Header component to TitleBar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Header from "$lib/components/Header.svelte";
+  import TitleBar from "$lib/components/TitleBar.svelte";
 </script>
 
 <div
   class="dark h-screen w-screen bg-background text-foreground overflow-hidden select-none grid grid-rows-[auto_1fr_auto] grid-cols-[240px_1fr]"
 >
-  <Header />
+  <TitleBar />
 </div>
